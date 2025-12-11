0200028e8456a1d126dd78081a559dfa5e25a8b7
paka666 <134531582+paka666@users.noreply.github.com>
2025-12-11 14:24:00 +0800

    Update merge-srs.sh

--- sing-box/merge-srs.sh
+++ sing-box/merge-srs.sh
@@ -2240,7 +2240,7 @@ process_and_inject_srs() {
           log_warn "    ⚠️  目标数组 $json_var 不存在，跳过注入"
         fi
       else
-        # [新增] 空集合跳过日志
+        # 空集合跳过日志
         log_info "  ⏭️  跳过 Group: $name (未配置 SRS 源)"
       fi
       unset -n srs_array
